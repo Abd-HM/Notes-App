0244a78fa12ee640f3adff179fee40a9b4e6ba3e
Abd-HM <aboobida2135291@gmail.com>
2023-09-30 16:59:54 +0300

    create custome note item

---
+++ lib/views/widgets/custome_note_item.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class NoteItem extends StatelessWidget {
+  const NoteItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
+      decoration: const BoxDecoration(
+          color: Color(0xffFFCC80),
+          borderRadius: BorderRadius.all(Radius.circular(16))),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            title: const Text(
+              'Flutter Note',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 26,
+              ),
+            ),
+            subtitle: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+              child: Text(
+                'Some trash text that no body cares about',
+                style: TextStyle(
+                  color: Colors.black.withOpacity(.5),
+                  fontSize: 18,
+                ),
+              ),
+            ),
+            trailing: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  FontAwesomeIcons.trash,
+                  size: 24,
+                  color: Colors.black,
+                )),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 24.0),
+            child: Text(
+              'Mat 24, 2023',
+              style:
+                  TextStyle(color: Colors.black.withOpacity(.4), fontSize: 16),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import 'custome_app_bar.dart';
+import 'custome_note_item.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
@@ -17,59 +18,6 @@ class NotesViewBody extends StatelessWidget {
           ),
           CustomeAppBar(),
           NoteItem(),
-        ],
-      ),
-    );
-  }
-}
-
-class NoteItem extends StatelessWidget {
-  const NoteItem({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
-      decoration: const BoxDecoration(
-          color: Color(0xffFFCC80),
-          borderRadius: BorderRadius.all(Radius.circular(16))),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: const Text(
-              'Flutter Note',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 26,
-              ),
-            ),
-            subtitle: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16.0),
-              child: Text(
-                'Some trash text that no body cares about',
-                style: TextStyle(
-                  color: Colors.black.withOpacity(.5),
-                  fontSize: 18,
-                ),
-              ),
-            ),
-            trailing: IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  FontAwesomeIcons.trash,
-                  size: 24,
-                  color: Colors.black,
-                )),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 24.0),
-            child: Text(
-              'Mat 24, 2023',
-              style:
-                  TextStyle(color: Colors.black.withOpacity(.4), fontSize: 16),
-            ),
-          ),
         ],
       ),
     );
